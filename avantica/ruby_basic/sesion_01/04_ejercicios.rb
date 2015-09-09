c05c75601653e0e4ac341f056ea36bd5364c8a53
Jose Flores <josealbertoflores@gmail.com>
2015-09-09 17:12:26 -0500

    sesion_01: Ejercicios y homework resueltos

--- avantica/ruby_basic/sesion_01/04_ejercicios.rb
+++ avantica/ruby_basic/sesion_01/04_ejercicios.rb
@@ -9,3 +9,19 @@ end
 # 2. Dado las edades [23, 12, 19, 15] obtenga aquellos mayores de edad
 [23, 12, 19, 15].each{ |n| puts n if n >= 18 } # para imprimir
 mayores_edad = [23, 12, 19, 15].select{ |n| n >= 18 } # para asignar a una variable
+
+# 3. Dado el arreglo ['java', 'php', 'ruby'] convierta los elementos en mayusculas
+lenguajes = ['java', 'php', 'ruby'].map{ |lenguaje| lenguaje.upcase}
+
+# 4. Dado el arreglo [5, 3, 9, 15] obtenga la suma total
+suma = [5, 3, 9, 15].inject{ |suma, n| suma += n }
+
+# 5. Cree un hash con los niveles y conocimientos que tenga
+lenguajes = {
+	basic: ['php', 'perl'],
+	intermediate: ['java']
+}
+
+# Agregue un nuevo conocimiento
+lenguajes[:advanced] = ['ruby']
+lenguajes.merge!( { :advanced => ['ruby'] })
--- avantica/ruby_basic/sesion_01/homework.rb
+++ avantica/ruby_basic/sesion_01/homework.rb
@@ -3,3 +3,25 @@ require 'json'
 archivo = File.read('homework.json')
 examen = JSON.parse(archivo);
 aciertos = 0
+
+examen.each do |item|
+  puts "#{item['nro']}. #{item['pregunta']}"
+  item['opciones'].each do |key, value|
+    puts "   #{key}) #{value}"
+  end
+  print '   Ingrese su respuesta (a,b,c,d): '
+  respuesta = gets.chomp.downcase
+  if item['respuesta'] == respuesta
+    puts '   Respuesta correcta!'
+    aciertos += 1
+  else
+    puts "   Respuesta incorrecta, la respuesta correcta era la alternativa #{item['respuesta']}"
+  end
+end
+
+puts ''
+score = aciertos.to_f / examen.length * 100
+puts "Respuestas correctas   : #{aciertos}"
+puts "Respuestas incorrectas : #{examen.length - aciertos}"
+puts "Score obtenido         : #{score.to_i}%"
+puts 'Resultado              : ' << (score >= 80 ? 'FELICITACIONES! Pasaste el examen!' : 'Necesitas estudiar mas')
